8d08b46e0d9f0c2cce243f17c755d7f66259362d
Lan Kim Ho <lan.ho@asnet.com.vn>
2024-05-08 10:02:42 +0700

    init

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,19 @@
+import clsx from "clsx";
+
 export default function Page() {
-  return <p>Customers Page</p>;
+  return (<div>
+    <p>Customers Page</p>
+    <div className='relative flex flex-col gap-2'>
+      <div className='absolute top-2 left-3 flex items-center'>Search</div>
+      <input
+        className={clsx(
+          ` w-full h-10 outline-0 py-1.5 px-4 text-textDefault text-sm ring-1  focus:ring-mainColor pl-20`,
+          
+        )}
+        placeholder={''}
+      />
+    </div>
+  
+  </div>
+);
 }
