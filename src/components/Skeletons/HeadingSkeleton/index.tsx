fef18b948a528089e187554f224b5b0627ef507c
Lan Kim Ho <lan.ho@asnet.com.vn>
2024-05-28 16:27:31 +0700

    update HeadingSkeleton

--- src/components/Skeletons/HeadingSkeleton/index.tsx
+++ src/components/Skeletons/HeadingSkeleton/index.tsx
@@ -3,7 +3,20 @@ import { Box, Skeleton } from '@mui/material';
 
 const HeadingSkeleton = (): JSX.Element => {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '10px' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '10px',
+        paddingTop: {
+          xs: '20px',
+          sm: '20px',
+          md: '32px',
+        },
+      }}
+    >
       <Skeleton variant="rectangular" width={122} height={42} />
       <Skeleton variant="rectangular" width={200} height={42} />
     </Box>
