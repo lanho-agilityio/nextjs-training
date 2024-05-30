3ec8182cba1628dc3a4b751f29bb286629abd53a
Lan Kim Ho <lan.ho@asnet.com.vn>
2024-05-30 14:28:42 +0700

    revert

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -10,16 +10,14 @@ import { Post } from '@/models';
 
 interface PostCardProps {
   content: Post;
-  isRecentPost?: boolean;
-  isArchived?: boolean;
 }
 
-const PostCard = ({ content, isRecentPost, isArchived = false }: PostCardProps): JSX.Element => {
+const PostCard = ({ content }: PostCardProps): JSX.Element => {
   const { id, title, tag, user, updatedAt, imageBase64 } = content;
 
   return (
     <Box sx={{ width: 'full', display: 'flex', flexDirection: 'column' }}>
-      <PostCardImage src={imageBase64} alt={title} to={id} isRecentPost={isRecentPost} isArchived={isArchived} />
+      <PostCardImage src={imageBase64} alt={title} to={id} />
       <PostCardContent id={id} title={title} tag={tag} author={user} updatedAt={updatedAt} />
     </Box>
   );
