58583b0be923c5b1b9944bee41da6627fae0c2c5
Lan Kim Ho <lan.ho@asnet.com.vn>
2024-05-31 14:35:22 +0700

    fix post image size for recent post

--- src/components/PostCard/PostCardImage/index.tsx
+++ src/components/PostCard/PostCardImage/index.tsx
@@ -25,7 +25,7 @@ const PostCardImage = ({ alt, src, to, isRecentPost = false, isArchived = false 
           width: { xs: '90vw', sm: '42vw', md: '45vw', lg: !isArchived && isRecentPost ? '45vw' : '33vw' },
           height: '300px',
           maxWidth: {
-            lg: !isArchived && isRecentPost ? '500px' : '300px',
+            lg: !isArchived && isRecentPost ? '470px' : '300px',
             md: '452px',
             sm: '350px',
             xs: '470px',
