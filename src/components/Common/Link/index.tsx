24a08782d4cd3c2b1e18bf9fcdb109445b943688
Lan Kim Ho <lan.ho@asnet.com.vn>
2024-05-31 14:59:51 +0700

    fix import for Link props in next/link

--- src/components/Common/Link/index.tsx
+++ src/components/Common/Link/index.tsx
@@ -1,14 +1,14 @@
 import { AnchorHTMLAttributes, ReactNode, memo } from 'react';
-import Link from 'next/link';
+import Link, { LinkProps } from 'next/link';
 import { Box, SxProps, Theme } from '@mui/material';
 
-interface LinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+interface CustomLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement>, LinkProps {
   href: string;
   children: ReactNode;
   linkStyle?: SxProps<Theme> | undefined;
 }
 
-const CustomLink = ({ href, children, linkStyle, ...rest }: LinkProps): JSX.Element => {
+const CustomLink = ({ href, children, linkStyle, ...rest }: CustomLinkProps): JSX.Element => {
   return (
     <Link href={href} style={{ textDecoration: 'none' }} {...rest}>
       <Box sx={{ ...linkStyle }}>{children}</Box>
