fe35696cda71ec868a68702dac3ac8e2a0326db7
Lan Kim Ho <lan.ho@asnet.com.vn>
2024-06-03 14:54:18 +0700

    fix error in tagService

--- src/services/tagService.ts
+++ src/services/tagService.ts
@@ -7,7 +7,7 @@ import { API_ROUTES, ERROR_MESSAGES, VALIDATE_TAGS } from '@/constants';
 export const queryAllCategory = async () => {
   let errorMessage = '';
   const url = `${API_ROUTES.TAGS}`;
-  const response = await APIs.get(url, VALIDATE_TAGS.TAGS).catch((error) => {
+  const response = await APIs.get(url, [VALIDATE_TAGS.TAGS]).catch((error) => {
     errorMessage = error || ERROR_MESSAGES.DEFAULT_API_ERROR;
   });
 
