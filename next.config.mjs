cdae05250a46406de62bd5ff4cff9e24c9ac25e5
Lan Kim Ho <lan.ho@asnet.com.vn>
2024-06-03 14:52:44 +0700

    feature/upload-pic-by-link-and-load

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    images: {
+        domains: ['localhost'],
+        loader: 'custom',
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+              },
+              {
+                protocol: 'http',
+                hostname: '**',
+              },
+        
+        ],
+      },
 };
 
 export default nextConfig;
--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -15,11 +15,17 @@ interface PostCardProps {
 }
 
 const PostCard = ({ content, isArchived, isRecentPost }: PostCardProps): JSX.Element => {
-  const { id, title, tag, user, updatedAt, imageBase64 } = content;
+  const { id, title, tag, user, updatedAt, imageBase64, imageName } = content;
 
   return (
     <Box sx={{ width: 'full', display: 'flex', flexDirection: 'column' }}>
-      <PostCardImage src={imageBase64} alt={title} to={id} isRecentPost={isRecentPost} isArchived={isArchived} />
+      <PostCardImage
+        src={imageBase64 || imageName}
+        alt={title}
+        to={id}
+        isRecentPost={isRecentPost}
+        isArchived={isArchived}
+      />
       <PostCardContent id={id} title={title} tag={tag} author={user} updatedAt={updatedAt} />
     </Box>
   );
